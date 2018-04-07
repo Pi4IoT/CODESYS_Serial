146824ab576b5416f9a106274efd7d12db1cf0d7
Pi4IoT <36192933+Pi4IoT@users.noreply.github.com>
2018-04-07 22:14:49 +0200

    Add files via upload

---
+++ Example_SPS.ino
@@ -0,0 +1,30 @@
+
+#include "High_Temp.h"
+#include <Grove_LED_Bar.h>
+
+Grove_LED_Bar bar(3, 2, 1);  // Clock pin, Data pin, Orientation
+HighTemp ht(A1, A0);
+int Temp;
+char inChar;
+
+void setup()
+{
+  Serial.begin(9600);
+  ht.begin();
+  bar.begin();
+}
+
+void loop()
+{
+  if (Serial.available() > 0) {
+    inChar = Serial.read();
+    if (inChar == 'g'){
+        Temp = ht.getThmc();
+        Serial.println(Temp);
+        }
+     if (isHexadecimalDigit(inChar)) {
+         bar.setLevel(int(inChar)-48);
+         //Serial.println(inChar);
+     }
+  }
+}
